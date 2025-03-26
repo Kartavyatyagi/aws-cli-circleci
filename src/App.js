cfc6dcac709e408781dcce4fa879c0365667975b
Kartavya Tyagi <kartavyatyagi453@gmail.com>
2025-03-26 17:33:08 +0530

    added artifacts.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>hello ji aman bhaishaab</h1>
+          <h1>hello ji aman bhaishaab ji</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
